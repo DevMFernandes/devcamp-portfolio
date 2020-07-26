4c7f87831961cb6e89213cbcfd425d95d68b62d7
Marcos Fernandes <contato@marcos-fernandes.com>
2020-07-26 15:49:19 -0300

    created scope for a couple of portfolio subtitles and updated seeds

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,9 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :description, :img, :thumb
+
+    def self.angular
+        where(subtitle: "Angular")
+    end
+
+    scope :ror, -> { where(subtitle: "Ruby on Rails") }
 end
