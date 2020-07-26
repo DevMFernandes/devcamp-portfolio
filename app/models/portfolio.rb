96596fb5c3c9ad03c5d974d8289900e8792cb01b
Marcos Fernandes <contato@marcos-fernandes.com>
2020-07-26 17:59:17 -0300

    added defaults to portfolio images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
     end
 
     scope :ror, -> { where(subtitle: "Ruby on Rails") }
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.img ||= "https://placehold.it/600x400"
+        self.thumb ||= "https://placehold.it/350x200"
+    end
 end
