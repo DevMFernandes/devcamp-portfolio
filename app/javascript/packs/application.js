a097be61e409171fcd1be71c08ff279048873509
Marcos Fernandes <contato@marcos-fernandes.com>
2020-08-19 14:53:05 -0300

    Front end sortable items in portfolio implemented

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,14 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import '../jquery-ui'
+     
+$(
+        () => {
+                $('.sortable').sortable()
+        }
+)
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
