3b8326496b63e3d6c5d4449ed7bbaa8015a490e8
Marcos Fernandes <contato@marcos-fernandes.com>
2020-08-18 23:48:05 -0300

    Refactored nav links helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,55 @@
 module ApplicationHelper
-    def login_helper style = ''
-       if current_user.is_a?(GuestUser)
-        (link_to "Login", new_user_session_path, class: style) + " " +
-        (link_to "Register", new_user_registration_path, class: style)        
-       else
-        (link_to "Edit", edit_user_registration_path, class: style) + " " +
-        (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
-       end
+  def login_helper style = ''
+    if current_user.is_a?(GuestUser)
+    (link_to "Login", new_user_session_path, class: "#{style} #{active? new_user_session_path}") + " " +
+    (link_to "Register", new_user_registration_path, class: "#{style} #{active? new_user_registration_path}")        
+    else
+    (link_to "Edit", edit_user_registration_path, class: "#{style} #{active? edit_user_registration_path}") + " " +
+    (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
+    end
+  end
+
+  def session_helper
+    if session[:source]
+        content_tag(:p, "Thanks for visiting me from #{session[:source]}")
+    end
+  end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: "Home"
+      },
+      {
+        url: contact_path,
+        title: "Contact"
+      },
+      {
+        url: about_path,
+        title: "About"
+      },
+      {
+        url: portfolios_path,
+        title: "Portfolio"
+      },
+      {
+        url: blogs_path,
+        title: "Blog"
+      },
+    ]
+  end
+
+  def nav_helper style, tag
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag}>"
     end
 
-    def session_helper
-        if session[:source]
-            content_tag(:p, "Thanks for visiting me from #{session[:source]}")
-        end
-    end
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
